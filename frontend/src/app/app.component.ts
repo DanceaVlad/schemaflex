ee783941c01b2aa9598c766433624b867e139bd3
Dancea <vlad.dancea@codesio.com>
2025-04-16 23:06:30 +0200

    Frontend: Show available schemas as mat-tabs

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
 
+import { MatTabsModule } from '@angular/material/tabs';
+
 import { DocumentSchema } from './data/DocumentSchema';
 import { SchemaService } from './services/schema.service';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, MatTabsModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
