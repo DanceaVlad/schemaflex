218635e8d75c92ca84c1a843763705aa5dae4e30
Dancea <vlad.dancea@codesio.com>
2025-04-20 23:02:59 +0300

    Both: simplify UI schema

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
 
-import { DateAdapter, MatNativeDateModule } from '@angular/material/core';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatTabsModule } from '@angular/material/tabs';
 
 import { JsonFormsModule } from '@jsonforms/angular';
 import { JsonFormsAngularMaterialModule, angularMaterialRenderers } from '@jsonforms/angular-material';
-import { Tester, and, createAjv, isControl, optionIs, rankWith, schemaTypeIs, scopeEndsWith } from '@jsonforms/core';
-import { parsePhoneNumber } from 'libphonenumber-js';
 
-import { CustomAutocompleteControlRenderer } from './custom.autocomplete';
-import { DataDisplayComponent } from './data.control';
 import { DocumentSchema } from './data/DocumentSchema';
-import { LangComponent } from './lang.control';
 import { SchemaService } from './services/schema.service';
 
 @Component({
@@ -24,6 +19,8 @@ import { SchemaService } from './services/schema.service';
 })
 export class AppComponent implements OnInit {
     documentSchemas: DocumentSchema[] = [];
+
+    renderers = angularMaterialRenderers;
 
     data = {
         orders: [
@@ -57,43 +54,7 @@ export class AppComponent implements OnInit {
         ],
     };
 
-    departmentTester: Tester = and(schemaTypeIs('string'), scopeEndsWith('department'));
-
-    renderers = [
-        ...angularMaterialRenderers,
-        { tester: rankWith(5, this.departmentTester), renderer: CustomAutocompleteControlRenderer },
-        {
-            renderer: DataDisplayComponent,
-            tester: rankWith(6, and(isControl, scopeEndsWith('___data'))),
-        },
-        {
-            renderer: LangComponent,
-            tester: rankWith(6, and(isControl, optionIs('lang', true))),
-        },
-    ];
-    i18n = { locale: 'de-DE' };
-    dateAdapter: any;
-    ajv = createAjv({
-        schemaId: 'id',
-        allErrors: true,
-    });
-
-    constructor(
-        private schemaService: SchemaService,
-        dateAdapter: DateAdapter<Date>
-    ) {
-        this.ajv.addFormat('time', '^([0-1][0-9]|2[0-3]):[0-5][0-9]$');
-        this.dateAdapter = dateAdapter;
-        dateAdapter.setLocale(this.i18n.locale);
-        this.ajv.addFormat('tel', (maybePhoneNumber) => {
-            try {
-                parsePhoneNumber(maybePhoneNumber, 'DE');
-                return true;
-            } catch (_) {
-                return false;
-            }
-        });
-    }
+    constructor(private schemaService: SchemaService) {}
 
     ngOnInit(): void {
         this.schemaService.getAllDocumentSchemas().subscribe({
